ae4dd914c24c1870eba43072b490b6b762bf88c2
HSSJW <okqkfkfk@naver.com>
2025-10-10 15:36:12 +0900

    Chore : API 공통 응답코드 설정

--- src/main/java/anam_145/SpringBoot/Server/apiPayload/code/ErrorReasonDTO.java
+++ src/main/java/anam_145/SpringBoot/Server/apiPayload/code/ErrorReasonDTO.java
@@ -1,16 +1,17 @@
 package anam_145.SpringBoot.Server.apiPayload.code;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ErrorReasonDTO {
     @JsonIgnore
-    private HttpStatus httpStatus;
-
+    private final HttpStatus httpStatus;
     private final boolean isSuccess;
     private final String code;
     private final String message;
