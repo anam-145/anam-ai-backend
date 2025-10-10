ebdcaec9f0ac77ca6c594ec68d1fadc091cf6e6a
HSSJW <okqkfkfk@naver.com>
2025-10-10 15:51:49 +0900

    Chore : 공통 예외처리 코드 추가

---
+++ src/main/java/anam_145/SpringBoot/Server/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package anam_145.SpringBoot.Server.apiPayload.exception;
+
+import anam_145.SpringBoot.Server.apiPayload.code.BaseErrorCode;
+import anam_145.SpringBoot.Server.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
